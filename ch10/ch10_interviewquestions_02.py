ab9adbd04b2a07f5f7d0a877c1d7da6a8646bb9e
Jason Boyd <jason.boyd.ce@gmail.com>
2016-09-18 11:27:18 -0700

    Added solution to 10.2

---
+++ ch10/ch10_interviewquestions_02.py
@@ -0,0 +1,12 @@
+# 10.2 Group Anagrams: Write a method to sort an array of strings so that all
+# the anagrams are next to each other.
+def groupAnagrams(words, inPlace=False):
+	# If we are not sorting in place then copy 'words' to a new list.
+	if inPlace == False:
+		words = words[:]
+		
+	# Map each word to a key that can be used to sort the words.  In this case
+	# the key is just the word sorted 
+	words.sort(key = lambda word: (''.join(sorted(word)), word))
+	
+	return words
