e18a9758860ea7194cf9571cb65c07b5addf14d6
Jason Boyd <jason.boyd.ce@gmail.com>
2016-09-18 11:31:14 -0700

    Fixed comment

--- ch10/ch10_interviewquestions_02.py
+++ ch10/ch10_interviewquestions_02.py
@@ -6,7 +6,8 @@ def groupAnagrams(words, inPlace=False):
 		words = words[:]
 		
 	# Map each word to a key that can be used to sort the words.  In this case
-	# the key is just the word sorted 
+	# the key is just the word with the letters in alphabetical order.  Words
+	# with the same key are anagrams of each other.
 	words.sort(key = lambda word: (''.join(sorted(word)), word))
 	
 	return words
