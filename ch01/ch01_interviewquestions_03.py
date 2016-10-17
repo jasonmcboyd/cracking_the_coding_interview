2a4d9d5b291464a5e9e93acc14ec212a2fcd630d
Jason Boyd <jason.boyd.ce@gmail.com>
2016-10-16 20:46:51 -0700

    Updated comment

--- ch01/ch01_interviewquestions_03.py
+++ ch01/ch01_interviewquestions_03.py
@@ -1,3 +1,7 @@
-# 1.3
+# 1.3 URLif: Write a method to replace all spaces in a string with '%20'.  You
+# may assume that the string has sufficient space at the end to hold the
+# additional characters, and that you are given the "true" length of the
+# string.  (Note: If implementing in Java, please use a character array so that
+# you can perform this operation in place.)
 def urlify(input):
 	return [x if x != ' ' else ['%', '2', '0'] for x in input]
