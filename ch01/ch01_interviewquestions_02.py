ff675fa15842be91ce65dd7194535e2cc8ec89f4
Jason Boyd <jason.boyd.ce@gmail.com>
2016-11-02 13:04:55 -0700

    Update ch01_interviewquestions_02.py
    Added optimizations

--- ch01/ch01_interviewquestions_02.py
+++ ch01/ch01_interviewquestions_02.py
@@ -3,18 +3,43 @@
 from collections import defaultdict
 
 def arePermutations(first, second):
+	# If the two strings are not the same length then 
+	# they are not permutations of each other.
+	if len(first) != len(second):
+		return False
+	
+	# Create a default dictionary to track the number
+	# of occurrences of each character
 	counter = defaultdict(int)
 	
+	# Count all of the characters in the first string.
 	for i in first:
 		counter[i] += 1
 	
+	# For every character in the second string decrement
+	# the count of that character in the dictionary.
+	# If the resulting count is less than zero then we
+	# have found a character in the second string that
+	# was not in the first string.
+	#
+	# But what happens if there are fewer occurrences of 
+	# a character in the second string than the first, 
+	# you say.
+	# The early termination test only checks for the case
+	# when there are more of a particular character in the
+	# second string than the first, you say.
+	# Good point, but we know the two strings are the same
+	# length (see first test at the top of the method). 
+	# So if the second string has fewer of one character
+	# but the two strings are the same length then it must
+	# be the case that the second string has more of a
+	# different character.  We will eventually come across
+	# that character and terminate early.
 	for i in second:
 		counter[i] -= 1
 		if counter[i] < 0:
 			return False
-		
-	for i in counter.values():
-		if i != 0:
-			return False
-	
+			
+	# If we have not returned yet then the two strings
+	# are permutations.
 	return True
