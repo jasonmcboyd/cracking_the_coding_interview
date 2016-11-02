71a857a0d773fa79a425941d5d1fb0dfef88433b
Jason Boyd <jason.boyd.ce@gmail.com>
2016-11-02 12:49:55 -0700

    Update ch01_interviewquestions_01.py
    Added comment

--- ch01/ch01_interviewquestions_01.py
+++ ch01/ch01_interviewquestions_01.py
@@ -1,6 +1,8 @@
 # 1.1 Implement an algorithm to determine if a string has all unique characters
 def hasUniqueCharacters(input):
 	chars = set()
+	# Possible optimization would be to check the length after each add
+	# operation and return early.
 	for i in input:
 		chars.add(i)
 	return len(chars) == len(input)
